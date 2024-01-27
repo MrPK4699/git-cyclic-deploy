160cc191ee3f4cb224f06e895f75e7fb0a0c294a
Pankaj Khatri <4699pankajkhatri@gmail.com>
2024-01-27 22:05:06 +0530

    fin

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express= require('express');
+
+require('dotenv').config();
 const app=express();
 
 app.get('/welcome',(req, res)=>{
@@ -10,12 +12,13 @@ app.get('/goodbye',(req, res)=>{
 app.get('/name',(req, res)=>{
     res.send(`Hello My name is ${process.env.NAME}`)
 })
+// console.log(`Hello My name is ${process.env.NAME}`)
 
 const port = process.env.PORT;
 
 app.listen(port, ()=>{
     try {
-        console.log(('listinung') +port)
+        console.log(('listining ') +port)
     } catch (err) {
         console.log((err));
     }
